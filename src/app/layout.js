147322038eb914b955b2ec9172a40d099d683293
sadmansakibrashid <sadmansakibrashid@gmail.com>
2026-05-16 15:29:51 +0600

    fixing error from navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/Header";
-import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 
 const geistSans = Geist({
